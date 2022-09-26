5b93ea20ff973e268e3fde018d5be3dbb6456c6b
Zokerzes <romzes85@mail.ru>
2022-09-26 20:56:14 +0300

    Добавьте файлы проекта.

---
+++ tamagochi/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using Timer = System.Timers.Timer;
+namespace tamagochi
+{
+    
+    internal class Program
+    {
+        private static Timer aTimer;
+
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
+                              e.SignalTime);
+        }
+        private static void SetTimer()
+        {
+            // Create a timer with a two second interval.
+            aTimer = new System.Timers.Timer(2000);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+        static void Main(string[] args)
+        {
+            DialogResult r8 = MessageBox.Show("запустить таймер",
+                                   "Help Caption", MessageBoxButtons.OKCancel,
+                                   MessageBoxIcon.Question,
+                                   MessageBoxDefaultButton.Button2, 0,
+                                   //"mspaint.chm",
+                                   "mspaint.chm::/paint_brush.htm"
+                                   );
+            if(r8 == DialogResult.OK)
+            {
+                SetTimer();
+                Console.ReadLine();
+                aTimer.Stop();
+                aTimer.Dispose();
+                
+
+            }
+            else Console.WriteLine("Cancel");
+        }
+    }
+}
