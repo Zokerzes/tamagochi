0dfdaaffdb43c7b4f83a564839dd80eee3cc1bca
Zokerzes <romzes85@mail.ru>
2022-09-28 19:54:00 +0300

    ++

--- tamagochi/Program.cs
+++ tamagochi/Program.cs
@@ -12,41 +12,68 @@ namespace tamagochi
     
     internal class Program
     {
-        private static Timer aTimer;
+        static void Main(string []Args)
+        {
+            Toy toy = new Toy();
+        }
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //private static Timer aTimer;
 
-        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
-        {
-            Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
-                              e.SignalTime);
-        }
-        private static void SetTimer()
-        {
-            // Create a timer with a two second interval.
-            aTimer = new System.Timers.Timer(2000);
-            // Hook up the Elapsed event for the timer. 
-            aTimer.Elapsed += OnTimedEvent;
-            aTimer.AutoReset = true;
-            aTimer.Enabled = true;
-        }
-        static void Main(string[] args)
-        {
-            DialogResult r8 = MessageBox.Show("запустить таймер",
-                                   "Help Caption", MessageBoxButtons.OKCancel,
-                                   MessageBoxIcon.Question,
-                                   MessageBoxDefaultButton.Button2, 0,
-                                   //"mspaint.chm",
-                                   "mspaint.chm::/paint_brush.htm"
-                                   );
-            if(r8 == DialogResult.OK)
-            {
-                SetTimer();
-                Console.ReadLine();
-                aTimer.Stop();
-                aTimer.Dispose();
+        //private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        //{
+        //    Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
+        //                      e.SignalTime);
+        //}
+        //private static void SetTimer()
+        //{
+        //    // Create a timer with a two second interval.
+        //    aTimer = new System.Timers.Timer(2000);
+        //    // Hook up the Elapsed event for the timer. 
+        //    aTimer.Elapsed += OnTimedEvent;
+        //    aTimer.AutoReset = true;
+        //    aTimer.Enabled = true;
+        //}
+        //static void Main(string[] args)
+        //{
+        //    DialogResult r8 = MessageBox.Show("запустить таймер",
+        //                           "ТАМАГОЧИ", MessageBoxButtons.OKCancel,
+        //                           MessageBoxIcon.Question,
+        //                           MessageBoxDefaultButton.Button2, 0,
+        //                           //"mspaint.chm",
+        //                           "mspaint.chm::/paint_brush.htm"
+        //                           );
+        //    if(r8 == DialogResult.OK)
+        //    {
+        //        SetTimer();
+        //        Console.ReadLine();
+        //        aTimer.Stop();
+        //        aTimer.Dispose();
                 
 
-            }
-            else Console.WriteLine("Cancel");
-        }
+        //    }
+        //    else Console.WriteLine("Cancel");
+        //}
     }
 }
---
+++ tamagochi/Toy.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using Timer = System.Timers.Timer;
+
+namespace tamagochi
+{
+    internal class Toy
+    {
+        bool isAlife;
+        ToyArgs prevToyArgs;
+        Timer timer;
+        SortedDictionary <string, MessageBoxIcon> wishList = new SortedDictionary<string, MessageBoxIcon>()
+        {
+            {"Хочу есть",MessageBoxIcon.Warning },{"хочу спать",MessageBoxIcon.Stop},{"Я заболел",MessageBoxIcon.Warning },
+            {"Поиграешь со мной?",MessageBoxIcon.Question },{"Погуляешь со мной?",MessageBoxIcon.Question }
+        };
+
+
+        private void initTimer()
+        {
+            timer = new Timer(500);
+            timer.Elapsed += OnToyEvent;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+
+        public void OnToyEvent(Object source, ElapsedEventArgs e)
+        {
+            //Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss}",
+            //                  e.SignalTime);
+
+            timer.Enabled = false;
+
+            ToyArgs toyArgs = getWishList();
+
+            DialogResult result = MessageBox.Show(toyArgs.text, toyArgs.title, MessageBoxButtons.OKCancel, toyArgs.icon);
+            if (result == DialogResult.OK) timer.Enabled = true;
+            else
+            {
+                timer.Stop();
+                timer.Dispose();
+                isAlife = false;
+            }
+        }
+            public Toy()
+            {
+                isAlife = true;
+                prevToyArgs = null;
+                initTimer();
+                life();
+                getWishList();
+            }
+        private void life()
+        {
+            while (isAlife)
+            {
+
+            };
+        }
+
+        private ToyArgs getWishList()
+        {
+            ToyArgs toyArgs = null;
+            Random random = new Random();
+            int temp = random.Next(0, 4);
+            SortedDictionary<string, MessageBoxIcon>.KeyCollection keys = wishList.Keys;
+            List<string> keyList = keys.ToList();
+            //keyList.ForEach(k => Console.WriteLine(k));
+
+            MessageBoxIcon icon;
+            wishList.TryGetValue(keyList[temp],out icon);
+            toyArgs = new ToyArgs("Тамагочи", keyList[temp], icon);
+            return toyArgs;
+
+        }
+
+        private class ToyArgs
+        {
+            public string title, text;
+            public MessageBoxIcon icon;
+            public ToyArgs(string title, string text, MessageBoxIcon icon)
+            {
+                this.title = title;
+                this.text = text;
+                this.icon = icon;
+            }
+        }
+    }
+}
