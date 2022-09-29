ec11184fa6de946bc8164b0aecb8f2fa9ce92a2b
Bliznyuk Roman <romzes85@mail.ru>
2022-09-29 23:35:35 +0300

    add grafic worked

--- tamagochi/Program.cs
+++ tamagochi/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Timers;
 using System.Windows.Forms;
 using Timer = System.Timers.Timer;
+
 namespace tamagochi
 {
     
@@ -14,66 +15,9 @@ namespace tamagochi
     {
         static void Main(string []Args)
         {
-            Toy toy = new Toy();
+            
+            Toy toy = new Toy();        //старт программы
+            
         }
-       
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        //private static Timer aTimer;
-
-        //private static void OnTimedEvent(Object source, ElapsedEventArgs e)
-        //{
-        //    Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
-        //                      e.SignalTime);
-        //}
-        //private static void SetTimer()
-        //{
-        //    // Create a timer with a two second interval.
-        //    aTimer = new System.Timers.Timer(2000);
-        //    // Hook up the Elapsed event for the timer. 
-        //    aTimer.Elapsed += OnTimedEvent;
-        //    aTimer.AutoReset = true;
-        //    aTimer.Enabled = true;
-        //}
-        //static void Main(string[] args)
-        //{
-        //    DialogResult r8 = MessageBox.Show("запустить таймер",
-        //                           "ТАМАГОЧИ", MessageBoxButtons.OKCancel,
-        //                           MessageBoxIcon.Question,
-        //                           MessageBoxDefaultButton.Button2, 0,
-        //                           //"mspaint.chm",
-        //                           "mspaint.chm::/paint_brush.htm"
-        //                           );
-        //    if(r8 == DialogResult.OK)
-        //    {
-        //        SetTimer();
-        //        Console.ReadLine();
-        //        aTimer.Stop();
-        //        aTimer.Dispose();
-                
-
-        //    }
-        //    else Console.WriteLine("Cancel");
-        //}
     }
 }
--- tamagochi/Toy.cs
+++ tamagochi/Toy.cs
@@ -7,18 +7,25 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Timers;
 using System.Windows.Forms;
+using System.Diagnostics;
 using Timer = System.Timers.Timer;
+using static System.Console;
 
 namespace tamagochi
 {
     internal class Toy
     {
-        bool isAlife=true;
-        bool isSick=false;
-        int refusal =0;
-        ToyArgs prevToyArgs;
-        Timer timer;
-        Timer globalTimer;
+        bool isAlife=true;          //статус жив или нет
+        bool isSick=false;          //статус болеет или нет
+        int refusal =0;             //счётчик отказов
+        ToyArgs prevToyArgs;        //предыдущее сообщение чтобы не повторялось  
+        Timer timer;                //таймер хотелок
+        Timer globalTimer;          //таймер жизни
+        int timeOfLife = 30000;     //мксекунды
+        int wishlistPeriod = 1000;   //мксекунды
+        PrintToy printToy = new PrintToy(); 
+
+        //создаём коллекцию хотелок
         SortedDictionary<string, MessageBoxIcon> wishList = new SortedDictionary<string, MessageBoxIcon>()
         {
             {"Хочу есть",MessageBoxIcon.Warning },
@@ -28,79 +35,110 @@ namespace tamagochi
             {"Погуляешь со мной?",MessageBoxIcon.Question }
         };
 
+        
 
-        private void initTimer()
+        private void initTimer() //опредиляем таймер хотелок
         {
 
-            timer = new Timer(500);
+            timer = new Timer(wishlistPeriod);
             timer.Elapsed += OnToyEvent;
             timer.AutoReset = true;
             timer.Enabled = true;
         }
-
-        private void initGlibalTimer()
+       
+        private void initGlobalTimer() //опредиляем таймер жизни
         {
 
-            globalTimer = new Timer(120000);
-            timer.Elapsed += OnToyEvent;
-            timer.AutoReset = false;
-            timer.Enabled = true;
+            globalTimer = new Timer(timeOfLife);
+            globalTimer.Elapsed += OnSenility;
+            globalTimer.AutoReset = false;
+            globalTimer.Enabled = true;
         }
 
+        //событие: старость
+        public void OnSenility(Object source, ElapsedEventArgs e)
+        {
+            globalTimer.Stop();
+            globalTimer.Dispose();
+            timer.Stop();
+            timer.Dispose();
+            isAlife = false;
+        }
+
+        //событие: хотелка
         public void OnToyEvent(Object source, ElapsedEventArgs e)
         {
-            //Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss}",
-            //                  e.SignalTime);
-
             timer.Enabled = false;
-
             ToyArgs toyArgs = getWishList();
             while (toyArgs.Equals(prevToyArgs)) toyArgs = getWishList();
             prevToyArgs = toyArgs;
+            printToy.itsOk();
+            Console.WriteLine($"isSick = {isSick}, refusal = {refusal}");
             DialogResult result = MessageBox.Show(toyArgs.text, toyArgs.title, MessageBoxButtons.OKCancel, toyArgs.icon);
-            if (result == DialogResult.OK) timer.Enabled = true;
+            if (result == DialogResult.OK) { 
+                timer.Enabled = true;
+                printToy.itsOk();
+                Console.WriteLine($"isSick = {isSick}, refusal = {refusal}");
+            }
             else
             {
-                //timer.Stop();
-                //timer.Dispose();
-                timer.Enabled = true;
-                refusal ++;
-                if(refusal >=3) isSick = true;
+                printToy.itsNotOk();
                 Console.WriteLine($"isSick = {isSick}, refusal = {refusal}");
-               
+                timer.Enabled = true;
+                refusal++;
+                if (refusal >= 3) isSick = true;
+
+
                 if (isSick)
                 {
                     timer.Enabled = false;
+                    printToy.itsSick();
                     Console.WriteLine($"isSick = {isSick}, refusal = {refusal}");
                     DialogResult result1 = MessageBox.Show("Вылечи меня", "Тамогочи заболел", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
-                    if (result1 == DialogResult.OK) timer.Enabled = true;
+                    if (result1 == DialogResult.OK)
+                    {
+                        refusal = 0;
+                        isSick = false;
+                        timer.Enabled = true;
+                    }
                     else
                     {
+                        printToy.itsRIP();
+                        Console.WriteLine($"isSick = {isSick}, refusal = {refusal}");
+                        MessageBox.Show("RIP: из-за болезни.", "Тамогочи фсёёёоооо", MessageBoxButtons.OK, MessageBoxIcon.Error);
                         timer.Stop();
                         timer.Dispose();
                         isAlife = false;
-                        MessageBox.Show("RIP", "Тамогочи фсёёёоооо", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        
+
                     }
                 }
             }
-        }
-        public Toy()
-        {
-            isAlife = true;
-            prevToyArgs = null;
-
-            initTimer();
-            life();
-            getWishList();
         }
         private void life()
         {
             while (isAlife)
             {
-
+                
+                
             };
+            if (!isSick && !isAlife)
+            {
+                printToy.itsRIP();
+                Console.WriteLine($"isSick = {isSick}, refusal = {refusal}");
+                MessageBox.Show("RIP: от старости.", "Тамогочи фсёёёоооо", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
+        public Toy()
+        {
+            
+            isAlife = true;
+            prevToyArgs = null;
+            initGlobalTimer();
+            initTimer();
+            life();
+            getWishList();
+        }
+       
 
         private ToyArgs getWishList()
         {
@@ -141,6 +179,51 @@ namespace tamagochi
             public override int GetHashCode()
             {
                 return base.GetHashCode();
+            }
+        }
+        public class PrintToy
+        {
+            public void itsOk()
+            {
+                Console.Clear();
+                WriteLine(@"  _____  ");
+                WriteLine(@" /     \ ");
+                WriteLine(@" | O O | ");
+                WriteLine(@" |  #  | ");
+                WriteLine(@" | \_/ | ");
+                WriteLine(@" \_____/ ");
+            }
+
+            public void itsNotOk()
+            {
+                Console.Clear();
+                WriteLine(@"  _____  ");
+                WriteLine(@" /     \ ");
+                WriteLine(@" |  o O| ");
+                WriteLine(@" | #   | ");
+                WriteLine(@" |   _ | ");
+                WriteLine(@" \_____/ ");
+            }
+            public void itsSick()
+            {
+                Console.Clear();
+                WriteLine(@"  _____  ");
+                WriteLine(@" /     \ ");
+                WriteLine(@" | - - | ");
+                WriteLine(@" |  #  | ");
+                WriteLine(@" |  _  | ");
+                WriteLine(@" \ / \ / ");
+                WriteLine(@"  \___/ ");
+            }
+            public void itsRIP()
+            {
+                Console.Clear();
+                WriteLine(@"  _____  ");
+                WriteLine(@" /     \ ");
+                WriteLine(@" | X X | ");
+                WriteLine(@" |  #  | ");
+                WriteLine(@" |  _  | ");
+                WriteLine(@" \_____/ ");
             }
         }
     }
